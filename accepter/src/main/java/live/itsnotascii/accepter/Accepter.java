80142b3eac93a0a942a79f6b26a0adea26a04395
debattlinbeen <cjbeenham@gmail.com>
2019-11-08 23:06:56 +0000

    Accepter: print the tragedy

--- accepter/src/main/java/live/itsnotascii/accepter/Accepter.java
+++ accepter/src/main/java/live/itsnotascii/accepter/Accepter.java
@@ -1,11 +1,13 @@
 package live.itsnotascii.accepter;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.IncomingConnection;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.ContentTypes;
+import akka.http.javadsl.model.HttpEntities;
 import akka.http.javadsl.model.HttpMethods;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
@@ -16,8 +18,19 @@ import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
+import akka.util.ByteString;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.DirectoryIteratorException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 
@@ -76,6 +89,49 @@ public class Accepter {
 												.withEntity(CLEAR_SCREEN + "Hello " + name + "!\n");
 							} else if (uri.path().equals("/ping")) {
 								return HttpResponse.create().withEntity(CLEAR_SCREEN + "PONG!\n");
+							} else if (uri.path().equals("/thetragedy")) {
+								HashMap<Integer, byte[]> map = new HashMap<>();
+
+								Path dir = Paths.get("./accepter/src/main/resources/loading/thetragedy/");
+								try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+									for (Path file : stream) {
+										System.out.println(file.getFileName());
+										map.put(Integer.parseInt(file.getFileName().toString().replaceAll(".txt", "")),
+												Files.readAllBytes(file));
+									}
+								} catch (IOException | DirectoryIteratorException x) {
+									// IOException can never be thrown by the iteration.
+									// In this snippet, it can only be thrown by newDirectoryStream.
+									System.err.println(x);
+								}
+
+								System.out.println("Finished reading");
+
+								List<byte[]> s1 = new ArrayList<>(map.values());
+
+								StringBuilder sb = new StringBuilder();
+
+
+								for (byte[] bytes : s1) {
+									sb.append(bytes.length)
+											.append('\n')
+											.append(Arrays.toString(bytes))
+											.append('\n');
+								}
+
+								String fileName = "./accepter/src/main/resources/loading/thetragedy/full.txt";
+								Files.writeString(Paths.get(fileName), sb.toString(), Charset.defaultCharset());
+
+								int FPS = 24;
+								int framelength = s1.size();
+								long duration = framelength < 24 ? 1 : framelength / FPS;
+
+								Source<ByteString, NotUsed> source = Source.range(0, framelength - 1)
+										.map(str -> ByteString.fromString(CLEAR_SCREEN + new String(s1.get(str)) + "\n"))
+										.throttle(framelength, Duration.ofSeconds(duration * 4));
+
+								return HttpResponse.create()
+										.withEntity(HttpEntities.createChunked(ContentTypes.TEXT_PLAIN_UTF8, source));
 							} else {
 								return NOT_FOUND;
 							}
